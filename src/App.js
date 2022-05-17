a73f50a58aaec8e82b343e6f547c29473785a53e
niteshsingh29 <niteshsingh29072000a@gmail.com>
2022-05-17 22:37:45 +0530

    react-basic-buttons-test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import fetchdata from "./data";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState(fetchdata);
+  const [color, setColor] = useState("");
+  console.log(data);
+  // const handleClick = (i) => {
+  //   console.log("hey am clicked", i);
+  //   setColor(true);
+  // };
   return (
     <div className="App">
-    <div>hello world</div>
+      <div>
+        {data.map((item, i) => {
+          return (
+            <>
+              <div key={i} style={{ border: "1px solid red" }}>
+                <h3>{item.id}</h3>
+                <h1>{item.title}</h1>
+                <h3>{item.location}</h3>
+                <button
+                  style={{
+                    backgroundColor: color === item.id ? "red" : "yellow",
+                  }}
+                  onClick={() => setColor(item.id)}
+                >
+                  HELLO
+                </button>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    title: "nitesh",
+    location: "surat",
+  },
+  {
+    id: 2,
+    title: "baap developer",
+    location: "surat",
+  },
+  {
+    id: 3,
+    title: "jhatula",
+    location: "surat",
+  },
+  {
+    id: 4,
+    title: "chana",
+    location: "surat",
+  },
+];
+
+export default data;
